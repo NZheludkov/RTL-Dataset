0ee042f9e77ebde1bae91a8a1b2c4162983652cd
nvgel <nvgel@cs.niisi.ras.ru>
2026-02-05 00:46:38 +0300

    feat: add 7 new blocks

---
+++ designs/pci_bridge32/config.tcl
@@ -0,0 +1,13 @@
+set top_level "pci_bridge32"
+set clock "wb_clk_i"
+
+set CLK_PERIOD "9.0"
+set IO_DELAY "0.5"
+set CU "20"
+set AR "1.0"
+set PDN_HWIDTH "3"
+set PDN_HSPACING "1.6"
+set PDN_HPITCH "18"
+set PDN_VWIDTH "2"
+set PDN_VSPACING "1"
+set PDN_VPITCH "18"
---
+++ designs/sasc_top/config.tcl
@@ -0,0 +1,13 @@
+set top_level "sasc_top"
+set clock "clk"
+
+set CLK_PERIOD "5.0"
+set IO_DELAY "0.5"
+set CU "40"
+set AR "1.0"
+set PDN_HWIDTH "3"
+set PDN_HSPACING "1.6"
+set PDN_HPITCH "18"
+set PDN_VWIDTH "2"
+set PDN_VSPACING "1"
+set PDN_VPITCH "18"
