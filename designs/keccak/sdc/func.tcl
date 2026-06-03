1d7d08f1a3f660feb086294b6ad05daa6b0c10d9
nvgel <nickzheludkov@gmail.com>
2026-06-04 00:30:48 +0300

    feat: remove interfaces false path

--- designs/keccak/sdc/func.tcl
+++ designs/keccak/sdc/func.tcl
@@ -28,6 +28,6 @@ set_load -pin_load -min  $OUT_PORT_LOAD [all_outputs]
 set_input_transition -max $INPUT_TRANSITION [all_inputs]
 set_input_transition -min $INPUT_TRANSITION [all_inputs]
  
-set_false_path -from [all_inputs -no_clocks]
-set_false_path -to [all_outputs]
+#set_false_path -from [all_inputs -no_clocks]
+#set_false_path -to [all_outputs]
  
